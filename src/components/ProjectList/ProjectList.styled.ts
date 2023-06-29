97eb66c5bfe567be76d35c871e2a22d309d05706
AlexRudnyk <alexander.rudnyk@ukr.net>
2023-06-29 21:57:00 +0300

    adds projects page tablet size

--- src/components/ProjectList/ProjectList.styled.ts
+++ src/components/ProjectList/ProjectList.styled.ts
@@ -7,40 +7,55 @@ interface IProps {
 export const ProjectContainer = styled.ul`
   padding-top: 75px;
   display: flex;
-  gap: 50px;
+  /* gap: 50px; */
   flex-wrap: wrap;
+
+  @media screen and (min-width: 768px) {
+    gap: 30px;
+  }
+  @media screen and (min-width: 1280px) {
+    gap: 50px;
+  }
 `;
 
 export const ProjectListItem = styled.li<IProps>`
-  flex-basis: calc((100% - 2 * 50px) / 3);
+  /* flex-basis: calc((100% - 2 * 50px) / 3); */
 
-  -webkit-animation: scale-in-center 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94)
-    both;
-  animation: scale-in-center 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) both;
-  animation-delay: ${(props) => props.delay}s;
-
-  @-webkit-keyframes scale-in-center {
-    0% {
-      -webkit-transform: scale(0);
-      transform: scale(0);
-      opacity: 1;
-    }
-    100% {
-      -webkit-transform: scale(1);
-      transform: scale(1);
-      opacity: 1;
-    }
+  @media screen and (min-width: 768px) {
+    flex-basis: calc((100% - 1 * 30px) / 2);
   }
-  @keyframes scale-in-center {
-    0% {
-      -webkit-transform: scale(0);
-      transform: scale(0);
-      opacity: 1;
+
+  @media screen and (min-width: 1280px) {
+    flex-basis: calc((100% - 2 * 50px) / 3);
+
+    -webkit-animation: scale-in-center 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94)
+      both;
+    animation: scale-in-center 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) both;
+    animation-delay: ${(props) => props.delay}s;
+
+    @-webkit-keyframes scale-in-center {
+      0% {
+        -webkit-transform: scale(0);
+        transform: scale(0);
+        opacity: 1;
+      }
+      100% {
+        -webkit-transform: scale(1);
+        transform: scale(1);
+        opacity: 1;
+      }
     }
-    100% {
-      -webkit-transform: scale(1);
-      transform: scale(1);
-      opacity: 1;
+    @keyframes scale-in-center {
+      0% {
+        -webkit-transform: scale(0);
+        transform: scale(0);
+        opacity: 1;
+      }
+      100% {
+        -webkit-transform: scale(1);
+        transform: scale(1);
+        opacity: 1;
+      }
     }
   }
 `;
--- src/pages/Projects/Projects.styled.ts
+++ src/pages/Projects/Projects.styled.ts
@@ -14,4 +14,11 @@ export const ProjectsContainer = styled.div`
   margin-right: auto;
   margin-left: auto;
   padding: 20px 15px;
+
+  @media screen and (min-width: 768px) {
+    width: 738px;
+  }
+  @media screen and (min-width: 1280px) {
+    width: 1280px;
+  }
 `;
